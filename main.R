f2b26c16c6086e82d1c1118429e4a53260f32935
Gabriel Houdry--Boheme <gabriel.houdry--boheme@etudiant.univ-reims.fr>
2024-02-03 12:14:41 -0500

    Changed map type: first one is functional

--- main.R
+++ main.R
@@ -1,15 +1,53 @@
+# Chargement des packages nécessaires
+library(tidyverse)
+library(stringr)
+library(stringi)
+
 # Récupération du jeu de données
 dataset <- readxl::read_xlsx("dataset_elections.xlsx")
 summary(dataset)
+str(dataset)
+# Récupération de la carte de France
+map <- map_data("france")
 
-# Récupération des données départementales
-fra <- readRDS("Data/gadm36_FRA_2_sf.rds")
-mtq <- readRDS("Data/gadm36_MTQ_0_sf.rds")
-glp <- readRDS("Data/gadm36_GLP_0_sf.rds")
-reu <- readRDS("Data/gadm36_REU_0_sf.rds")
-myt <- readRDS("Data/gadm36_MYT_0_sf.rds")
-guy <- readRDS("Data/gadm36_GUF_0_sf.rds")
+# Nettoyage des données et aggrégation des résultats
+dataset <- dataset %>%
+  group_by(`Libellé du département`) %>%
+  summarise(total_votes = sum(Exprimés), 
+            total_blancs = sum(Blancs),
+            pourcentage_blanc = total_blancs / sum(Votants) * 100, 
+            total_abs = sum(as.numeric(Abstentions)), 
+            pourcentage_abs = total_abs / sum(as.numeric(Inscrits))* 100,
+            total_arthaud = sum(Voix), 
+            pourcentage_arthaud = total_arthaud / total_votes * 100) 
 
-# Modifier le nom des départements d'outre-mer (en français)
-guy$NAME_0 <- "Guyane"
-reu$NAME_0 <- "La Réunion"
+# Changement de noms de départements
+names(dataset)[1] <- "region"
+dataset$region <- stri_trans_general(dataset$region, "Latin-ASCII") %>%
+  str_replace_all("Cote-d'Or", "Cote-Dor") %>%
+  str_replace_all("Cotes-d'Armor", "Cotes-Darmor") %>%
+  str_replace_all("Corse-du-Sud", "Corse du Sud") %>%
+  str_replace_all("Val-d'Oise", "Val-Doise") %>%
+  str_replace_all("Corse-du-Sud", "Corse du Sud")
+
+map_theme <- theme(title=element_text(),
+                   plot.title=element_text(margin=margin(20,20,20,20), size=18, hjust = 0.5),
+                   axis.text.x=element_blank(),
+                   axis.text.y=element_blank(),
+                   axis.ticks=element_blank(),
+                   axis.title.x=element_blank(),
+                   axis.title.y=element_blank(),
+                   panel.grid.major= element_blank(), 
+                   panel.background= element_blank()) 
+
+result_map <- left_join(x = map[,-6], y = dataset)
+# Carte de test
+ggplot(result_map, aes(long,lat, group = group, fill = pourcentage_arthaud)) +
+  geom_polygon() +
+  coord_map() +
+  scale_fill_gradientn(colours = c("yellow","red"), name = "Pourcentage N. Arthaud") +
+  labs(x = "", 
+       y = "", 
+       title = "Résultats de N. Arthaud au premier tour des présidentielles 2022", 
+       subtitle = "Données via data.gouv") +
+  map_theme 
